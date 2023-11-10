420193a2cde767ddc6afc848614341db0bad5548
Vanir <jbolanosh@ucenfotec.ac.cr>
2023-11-09 23:13:51 -0600

    Route packege deleted on Transport Branch to avoid merging conflicts

--- src/Entities/Routes/ArrivalPoint.java
+++
@@ -1,7 +0,0 @@
-package Entities.Routes;
-
-public class ArrivalPoint {
-        private  String nameArrivalPoint;
-        private  String descriptionArrivalPoint;
-        private Coordinates_Point coordinatesArrivalPoint;
-}
