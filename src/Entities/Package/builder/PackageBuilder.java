16fa73b25e41fc736b1e36d4f10c084479b44f8e
cvsolsol <cinthiasanchoa@gmail.com>
2023-11-11 16:06:36 -0600

    Commit for package entity

---
+++ src/Entities/Package/builder/PackageBuilder.java
@@ -0,0 +1,60 @@
+package Entities.Package.builder;
+
+import  Entities.Package.components.PackageSize;
+import  Entities.Package.components.PackageWeight;
+import  Entities.Package.instanceEnum.PackageType;
+import  Entities.Package.classes.Package;
+
+public class PackageBuilder implements Builder{
+
+    //Type, Name, Description, Cost, Size, Weight
+
+    private PackageType packageType;
+    private String packageRecipientsName;
+    private String packageDescription;
+    private double packagePrice;
+    private PackageSize packageSize;
+    private PackageWeight packageWeight;
+
+    private String weightClassification;
+
+
+    @Override
+    public void setPackageType(PackageType packageType) {
+        this.packageType = packageType;
+    }
+
+    @Override
+    public void setPackageRecipientsName(String packageRecipientsName) {
+        this.packageRecipientsName = packageRecipientsName;
+    }
+
+    @Override
+    public void setPackageDescription(String packageDescription) {
+        this.packageDescription = packageDescription;
+    }
+
+    @Override
+    public void setPackagePrice(double packagePrice) {
+        this.packagePrice = packagePrice;
+    }
+
+    @Override
+    public void setPackageSize(PackageSize packageSize) {
+        this.packageSize = packageSize;
+    }
+
+    @Override
+    public void setPackageWeight(PackageWeight packageWeight) {
+        this.packageWeight = packageWeight;
+    }
+
+    @Override
+    public void setWeightClassification(String weightClassification) {
+        this.weightClassification = weightClassification;
+    }
+
+    public Package getResult() {
+        return new Package(packageType, packageRecipientsName, packageDescription,packagePrice, packageSize, packageWeight, weightClassification);
+    }
+}
---
+++ src/Entities/Package/instanceEnum/PackageType.java
@@ -0,0 +1,7 @@
+package Entities.Package.instanceEnum;
+
+public enum PackageType {
+
+  FRAGILE_PACKAGE, DOCUMENTS_PACKAGE, CLOTHES_PACKAGE
+
+}
