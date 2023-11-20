bf4264e703a4a8eb089bbe769dbfe3f8401b25d8
Vadak <105993197+Vadak11@users.noreply.github.com>
2023-11-20 01:53:56 -0600

    Delete project directory

--- project/src/main/java/com/laca/entity/Package/instanceEnum/PackageType.java
+++
@@ -1,7 +0,0 @@
-package com.laca.entity.Package.instanceEnum;
-
-public enum PackageType {
-
-  FRAGILE_PACKAGE, DOCUMENTS_PACKAGE, CLOTHES_PACKAGE
-
-}
--- project/src/main/java/com/laca/entity/Transport/AbstractCreator/TransportCreator.java
+++
@@ -1,7 +0,0 @@
-package com.laca.entity.Transport.AbstractCreator;
-
-import Entities.Transport.AbstractProduct.TransportUnit;
-
-public interface TransportCreator {
-    TransportUnit createTransport(double maxMeasureCapacity, String name, String plate, double height, double width, double maxWeight);
-}
