69cc9dc702dbda792a6e70e09993a240ac04da7e
Vadak <jbolanosh@ucenfotec.ac.cr>
2023-12-03 12:08:43 -0600

    Deleted.

--- src/Entities/Package/instanceEnum/PackageType.java
+++
@@ -1,7 +0,0 @@
-package Entities.Package.instanceEnum;
-
-public enum PackageType {
-
-  FRAGILE_PACKAGE, DOCUMENTS_PACKAGE, CLOTHES_PACKAGE
-
-}
--- src/Entities/Users/AbstractCreator/UserCreator.java
+++
@@ -1,7 +0,0 @@
-package Entities.Users.AbstractCreator;
-
-import Entities.Users.AbstractProduct.User;
-
-public interface UserCreator {
-    User createUser(int ID, String userID, String fullName);
-}
