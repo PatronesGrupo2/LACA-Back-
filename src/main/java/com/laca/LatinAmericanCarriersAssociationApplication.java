c0653ca4d565841890c4ee591dd171ac75a8d4b3
Diego Sequeira <jsequeirag@ucenfotec.ac.cr>
2023-12-06 17:28:08 -0600

    Main restablish

--- src/main/java/com/laca/LatinAmericanCarriersAssociationApplication.java
+++ src/main/java/com/laca/LatinAmericanCarriersAssociationApplication.java
@@ -1,11 +1,14 @@
 package com.laca;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"domain"} )
 public class LatinAmericanCarriersAssociationApplication {
-
+    public static void main(String[] args) {
+        SpringApplication.run(LatinAmericanCarriersAssociationApplication.class, args);
+    }
 }
 
